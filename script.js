d082ec4e264a39c7e3bb21f66044bd1d53836cee
gutoFM <gutofisco@gmail.com>
2023-04-11 11:51:03 -0300

    aula

---
+++ script.js
@@ -0,0 +1,22 @@
+/* WHILE
+let cont=0
+while(cont < 10){
+    document.write("O número é: " +cont);
+    document.write("<br>")
+    cont++
+}
+
+//DO WHILE
+let cont1 = 0
+do{
+    document.write("<br><br>")
+    document.write("O número do while é: " +cont1);
+    document.write("<br>")
+    cont1++
+}while(cont1 == 0)
+*/
+
+function mudar(){
+    let novo = document.getElementById("idNome")
+    document.getElementById("titulo").innerHTML = novo.value;
+}
